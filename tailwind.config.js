eb5ab3c72d7a5ae8f86c1a1de4df442663921cd1
leuzindev <leonardoc.soares08@gmail.com>
2023-05-30 21:01:31 -0300

    setup: inital setup, theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'blue-dark': '#1E6F9F',
+        blue: '#4EA8DE',
+        'purple-dark': '#5E60CE',
+        purple: '#8284FA',
+
+        'gray-700': '#0D0D0D',
+        'gray-600': '#1A1A1A',
+        'gray-500': '#262626',
+        'gray-400': '#333333',
+        'gray-300': '#808080',
+
+        'gray-200': '#D9D9D9',
+        'gray-100': '#F2F2F2',
+
+        danger: '#E25858',
+      },
+      spacing: {
+        12: '12px',
+        14: '14px',
+        16: '16px',
       },
     },
   },
